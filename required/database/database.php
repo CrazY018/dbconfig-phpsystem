9eb74d2c2d50a7962dc3050e5714875fa9ddbefe
CrazY018 <38430947+CrazY018@users.noreply.github.com>
2021-10-28 10:25:55 +0200

    Delete required/database directory

--- required/database/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-//////////////////////////////////////////////
-///////////// PLEASE DON'T TOUCH ////////////
-////////////////////////////////////////////
-$database_name = "devpoco_website";
-$database_host = "mysql-devpoco.alwaysdata.net";
-$database_user = "devpoco";
-$database_password = "SCANIAmatheo37";
-?>
